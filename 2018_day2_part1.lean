023854afe1d5235f989f5901172ae55d4e8ac538
Ethan Speer <70123091+jespeer@users.noreply.github.com>
2023-04-17 18:00:23 -0400

    Create 2018_day2_part1.lean

---
+++ 2018_day2_part1.lean
@@ -0,0 +1,291 @@
+def puzzle_input : String := 
+"uqyoeiznvmzrstpkgnsacjxwld
+uqloeizfvmbrstpkgndacjxwyd
+dqgoeizfbmbrstpkgnhacjxwld
+uqyjeizfvmbrstpygntacjxwld
+uqyoeizfhmbrstppbnhacjxwld
+uqyoeizfvmbrstpkschanjxwld
+uqyoeizfvtbrstpkgnmazjxwld
+uqyoeizfvmyrstpzgnhacjxsld
+udtoeizfvmbrttpkgnhacjxwld
+uqyneizfvmbratpognhacjxwld
+uqyoeizfvmbrstpkunhaajxnld
+uqyteizfvmbrstpkgnhzcnxwld
+hqyteuzfvmbrstpkgnhacjxwld
+uqyoeizyvmbrsdpkgnhacdxwld
+uqyoeizfibbrsepkgnhacjxwld
+uqxoeizfvmbrstpkgnhaxjjwld
+iqaoeizfvmbrstpkrnhacjxwld
+uqyoeizpvvbrstpkghhacjxwld
+aqyoeizfvmbrstpbgnhfcjxwld
+uqyonfzfvmbrstpkgnhacjgwld
+uqyoeizovmhrstpkgnhacjxwvd
+uqyoegzfvmbrstpkgnfacjxrld
+uqyoeizfvmbrstpjgnwacjxwdd
+uqyogiifumbrstpkgnhacjxwld
+uqyorizfvmbrstpkgnhaezxwld
+uqyoexdfvmbretpkgnhacjxwld
+uqyoeizfvmbrstpkgwhlcjxzld
+uqyoeibfvmbrstpggnhzcjxwld
+udyoeizfvmbrstppwnhacjxwld
+ufyoeizfvmhrstpkgnfacjxwld
+aqyoeftfvmbrstpkgnhacjxwld
+uhyoeivfvmbrstpkgnhadjxwld
+uqyoeizfvmbrstpkgmhacjxhud
+uqyfeizfvmbrstpkgnhkljxwld
+uqyoeibfvmbrstpkgbhazjxwld
+uqyceizfvnbrslpkgnhacjxwld
+uqyoeizfvmirstpkonhacaxwld
+fqyooizfvmbrstpkgnhacjxwlq
+uqydeizfvmbgstpugnhacjxwld
+uqyoiizfvmbrstpkgnhacjalld
+uqyivizfvmbrstpkgnhocjxwld
+uqyoeizfvmbrstpngnhjcjxwtd
+uqyoeizfvmbrstpkynhrcexwld
+ugmfeizfvmbrstpkgnhacjxwld
+uqyoeizrvmbrstpkwnhycjxwld
+uqyoeiyfvmbrstpkgnlacjxwwd
+unyoeizfvmbrstrkgnhadjxwld
+uqyoeizhvmbrstpkgnhacjawlz
+uqyoeazfvmbrstpwgnhacjxwlp
+uqyoeiznvmbastpkgnhaccxwld
+kqyoeisfvmbrstikgnhacjxwld
+uqyoeizfvlxrstpmgnhacjxwld
+uqyoeiofvmbrstpkgniacjxwll
+uqyoeizfvmbrswpegnhacixwld
+uqyoeizfvmarstlkgnhanjxwld
+uuyoeizfvmbrstykgnhacjxwbd
+uqyoeizfvmbpstxkgnhacjxwlu
+uqyoqizfvmbrsspkgnhacjpwld
+uqyoeizfvmzrstnkgnhacjgwld
+uqyoeizfvmbrstrcgnhacjxwud
+uqyoeizfvmbrstpkgnuacxxwsd
+cqyoeizfvmbrscpkgnhacjxwpd
+sqyoeizfaobrstpkgnhacjxwld
+uqyoeizfvmbrgtpkgnhacjxbwd
+uqyoeibhvmbrstpkgnhagjxwld
+uqyoeizfvcbrstpkgzhactxwld
+uqkoeizfvmbrstijgnhacjxwld
+uqroelzfvmbrstpkgmhacjxwld
+uqyoeizfvmbrsfpcgnhkcjxwld
+uqyoeizfumgrstpygnhacjxwld
+uqyozibfvmbrstpkgnhacjwwld
+uqyoeigfvmbrstpkgjharjxwld
+uqpoeizfvmbrstpkgnhacjxrcd
+uqyoeizfvmbrskokgnhqcjxwld
+uqyoeikfvmbrstpkgehacjxwle
+uqygeizfvhbrstpkgnqacjxwld
+lqyoeizfvmbrsvpkgnhacjuwld
+uqyjeizfvmbectpkgnhacjxwld
+unyoeizfvmbrstpkgnhzckxwld
+uqyoeizfvmbrstpkgmoacjxwlm
+uqfoeizfvmbrutpkgnhacjrwld
+uqyohizflnbrstpkgnhacjxwld
+uquoeizfembrstpkgniacjxwld
+uqyoeizfvmbrytpkgnhbcjkwld
+pqyoeizfvsbwstpkgnhacjxwld
+uqyoerzfvmbrstpkxntacjxwld
+uqyoeizfvmbritpkgnhacjqhld
+uqyoeisfvmbrstpkglhscjxwld
+hqyoeizfvmwrstpwgnhacjxwld
+uqyoeyzrvmbrstpkgnhacjxdld
+pqyoeozfombrstpkgnhacjxwld
+uwyosizfvmbrstpkgnhacjrwld
+uqyoegzfvmbrstekbnhacjxwld
+uqyoeinfvmirstpkgdhacjxwld
+uqyoeizfvmbistpkgnobcjtwld
+uqyoeikfvmbrsepkgnhycjxwld
+qqyoeiafvmbrstpkgnhacjxold
+uqbooizfvmbrstpkgnhacjxwdd
+uquoeizfvmbrswpkgnhatjxwld
+tqyoejzfvmbrstpkgnhacjxwlv
+yqyoeizfvmbrsttkgnhakjxwld
+usyoeizfvmbfstpkgnhkcjxwld
+gqyocizfvmbrstpkgnhacjbwld
+mqyoeizfvmbrstpkgnhacjzzld
+uqyoeczfvmbrotpkgnhacjxwsd
+jqyoeizfvmkrstpkgnhacaxwld
+uqyoeirfvmblstpkgnhaijxwld
+uqpoeirfvmbrstpkgbhacjxwld
+uqyoiizcvmbrsipkgnhacjxwld
+uqyoeizfvmbrstekgnhayjywld
+uqyoeizpvmnrstpkgnhacjxwcd
+eqyoaizfvmbrstpkgnhacjzwld
+uqyubizfvmbrltpkgnhacjxwld
+kqmoeizfvmbrstbkgnhacjxwld
+uqyoeizfvmkrstpvgnhahjxwld
+uqyoeizfrmbrsjpkgchacjxwld
+uqyoeizfvmbrstfkgnhacbxwlx
+uqyoeizfvmbrstpkbngayjxwld
+uqyorizfvmbrstpkgghacjxwle
+oqyoeizfvpkrstpkgnhacjxwld
+uqyoexzfvmbrscpkgnhacjxwlq
+uqkoyilfvmbrstpkgnhacjxwld
+uqyotizfamnrstpkgnhacjxwld
+uqnoeizfvmbrstpkgnhacdfwld
+uqyoeizffmbrstqkgnhacuxwld
+uqyoeizfvmbrzhpkgnhqcjxwld
+uqyzeizfvmbrsnpkgnracjxwld
+jhyoeizfvhbrstpkgnhacjxwld
+uqyohizfvmbrstpkgnqhcjxwld
+uqyleizfgmbrstlkgnhacjxwld
+ucyoeizzvmbrstgkgnhacjxwld
+uqyoeizqvmbrstbkgahacjxwld
+uqyseizpvkbrstpkgnhacjxwld
+uqyobizfvmirstpkgnhachxwld
+uqyoiizfvmbrstpkgnhacwxzld
+uqyoeizfvmbrstpkgnhafoxsld
+pqyoeizfvmbrstpkgnhaijxwle
+uqyoeizfvubrstpkgneacjxcld
+uqyoeigfumbrstpknnhacjxwld
+uqyoeiffvmbrstwkgnhacjxxld
+uqooeizfvmbrstpkgnhacjxilv
+uqyoeizfvmbrstpkgnhaljirld
+uqyoeizfvmbistpkgnoacjtwld
+uqyogizfvmlustpkgnhacjxwld
+uqyoeizfvmufjtpkgnhacjxwld
+uqyoeixfvmbrstpugwhacjxwld
+uqyoeizfrzbrsjpkgnhacjxwld
+uqyoeizfvmbbsqpkgzhacjxwld
+uqyoeizovqfrstpkgnhacjxwld
+uqyieizfvmbrsjpkgnhacjxwzd
+uqdoeizfvmbrstnkgnqacjxwld
+usyoohzfvmbrstpkgnhacjxwld
+uqyoeicfvmbrstpkgnhacgxwtd
+uqyoeizfvmbrstpkgkhbcjxold
+fwyoeizfvmbrstpkgnhkcjxwld
+oqyoeizfvmbrstpkenhacjjwld
+uqyoeizfvmbrttpkgjhacjhwld
+uqyoeituvmbrstpkgohacjxwld
+uqyoeizfvekrslpkgnhacjxwld
+upyoeizfvmbrstpkgnhjcjxwlb
+uqyoeiwfvmbrstpkgshacjmwld
+uqyceizfvmbrstpkgnpazjxwld
+aqyoeizfvmbrstpkgntacjxwed
+uqyosizfvmbrsapkgphacjxwld
+uqyoeizfvmbysipkgnhacjxwls
+uqyoeicfvmbgstpkgnhacjxwjd
+uqyoxizfvmbrsggkgnhacjxwld
+uqyoeizfvbbrhtplgnhacjxwld
+usyoeizfvmbrstvkgnhacjxwlo
+uqyleizfvmbrstpkgnhacjxwac
+uqyoeisfvmbrstpfgnhacjxwdd
+uqyoeizwvmbvstpkgnhacjxhld
+uqyoeczfvmbrstpkgnhecjxwlv
+uqyoeizfvmbrstpkgnhacjueid
+uqyoeisfvmbrstpkgnhaxjxwzd
+uqyoeizfvmbrstpkgnhicjmyld
+ugygeizfvmbrmtpkgnhacjxwld
+uoyoeiftvmbrstpkgnhacjxwld
+uqyoeinfvmbrstpkenhacjxwbd
+xqyoeiofvmbrstpkgnhacjxwlt
+uqyoeizfvnbrsipkgnhfcjxwld
+uqyteizfvmbrstpngmhacjxwld
+uqyoeijfvmbrstpdgnhacjcwld
+uqyoeizfjmbrstpkgntaclxwld
+uqyaeizfvmbrstpkgnhajjxwkd
+uqyoeizfvmbraqpkgnhacjxwlv
+uqyocizfvmbrstpkqnhrcjxwld
+uqyoqizfvmbrdtpkenhacjxwld
+uqyoeicfvwbrstpkknhacjxwld
+uqyoeizfvmbrstpktnhacjxxlz
+uqyoeizfvmbhstpkgnhacjmwgd
+uqyoeizfvmorskpkgnhncjxwld
+nqyxeezfvmbrstpkgnhacjxwld
+uqyoeitfvmbrstpksnhicjxwld
+uqyoeizfmmwrstpkgnhacjxwud
+uqyoeizfvmmrstpkgnhvcjxwlk
+uqyoeizfvmbrstpkgihacjxblz
+uqnoeizfvmwrstpkgnhaujxwld
+uqyoeizfvlbrsrpkgnhacjxwpd
+uqyoeizfvmbwstpkgnhvcjxwdd
+uqyoeixfvlbrstpkgxhacjxwld
+uqyoeizfvmbjstpmgnhacjxmld
+uqyoeizyvhbrstpkgnhacjxwzd
+uqyhepzfvmbrstpkghhacjxwld
+uqyoetzfvmbrstpkghsacjxwld
+uqyoeizfvmbrbnpkgnuacjxwld
+uqyokizfvmbrstpkgnhacqiwld
+uqyoeizfvmbrstpkgqhacagwld
+uqyoeizfvnbvstckgnhacjxwld
+uqyoeizfbmbhstpkjnhacjxwld
+uqyoeixfvmbrstpkgnegcjxwld
+uqyoeizfvmbrstpkshlacjxwld
+uqyoeisfhmbrsnpkgnhacjxwld
+uqyoekzfvmbrstpkgnhadjxsld
+uqyoeizfzmyrstckgnhacjxwld
+uqyoeizpvmbrztpkenhacjxwld
+uqyopizffmbrstpkgnhacjxuld
+uqyoeizfvzhrstpkgnhscjxwld
+uqyoeizfvlbrstskvnhacjxwld
+uqyohizfvmbrstekgnhacaxwld
+uqlodizfvmbastpkgnhacjxwld
+uqyoeizfvubrstpbgnhacjvwld
+uqnoupzfvmbrstpkgnhacjxwld
+tqzoeizfvmbrsdpkgnhacjxwld
+pqyobizfvmbrstphgnhacjxwld
+uqyweizfvkbrstpkguhacjxwld
+uqyoexzfvmbrstzktnhacjxwld
+cqyoeizfvmbrstpkgnhacvxfld
+uqyoeiefvmbrytpmgnhacjxwld
+uqyoeizfvmbhstpkgnhmcjpwld
+uqyoeizfvnbrstpkgnhaczrwld
+uqyoeibfvnbrstpkgnhacjxvld
+uqyoeizfvebrsypygnhacjxwld
+uqyoeikfnmbrstukgnhacjxwld
+uqyoeizfvmbrsfpkgnhfcdxwld
+uqyoeizfvmmrstpnbnhacjxwld
+uqyoeizfvmbrztpegnhacjxwrd
+pqyoeizfvmbrqtukgnhacjxwld
+uqyoeizfvmbbstpkgahacjxpld
+uqsoeizfvmbpstpkgnhatjxwld
+uqyodizfvmbrsfpkgnhgcjxwld
+uuyoeizfvmbnstpkgnhacjxbld
+gqyoeizfvmbrstpkgnhacjqwcd
+uqyoeizfvmyrotkkgnhacjxwld
+uqyoeizfvmabstpkgnhacjawld
+uqyoeizfvmbrstpkglhaszxwld
+ukyoeizfvmbrstpkgnhacjxtfd
+uqeoeizfvmbrstpkgnhtnjxwld
+uqyouizfvmnrstpkgxhacjxwld
+ugyoeizfvmbrsupygnhacjxwld"
+
+def splitString (s : String) (c : Char) : List String :=
+  (String.splitOn s (String.singleton c)).map String.trim
+
+def splitLines : List String := splitString puzzle_input '\n'
+
+#eval splitLines
+
+def has_double (s : String) : Bool := Id.run do
+  let chars := s.toList
+  let n := chars.length
+  for i in [:n] do
+    let mut count := 0
+    for j in [:n] do
+      if chars[i]! = chars[j]! then
+        count := count + 1
+    if count == 2 then
+      return true
+  false
+
+def has_triple (s : String) : Bool := Id.run do
+  let chars := s.toList
+  let n := chars.length
+  for i in [:n] do
+    let mut count := 0
+    for j in [:n] do
+      if chars[i]! = chars[j]! then
+        count := count + 1
+    if count == 3 then
+      return true
+  false
+
+
+def checksum (strings : List String) (checks : String → Bool) : Nat :=
+  (strings.filter checks).length
+
+def checksum_total (strings : List String) : Nat :=
+  checksum strings has_double * checksum strings has_triple
+
+#eval checksum_total splitLines
